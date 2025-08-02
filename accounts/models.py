c37c34badff8ca39dbd74e562dc9b7f7356b2bc9
YourGitHubUsername <jmwaura@byupathway.edu>
2025-08-02 05:03:35 +0300

    COMMIT

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    USER_TYPES = [
+        ('student', 'Student'),
+        ('instructor', 'Instructor'),
+        ('admin', 'Administrator'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=20, choices=USER_TYPES, default='student')
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.user_type}"
