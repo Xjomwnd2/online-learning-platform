d96a2e4c17fb6db3c4c9258079cb31f5616f670b
YourGitHubUsername <jmwaura@byupathway.edu>
2025-08-02 05:48:27 +0300

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .models import UserProfile
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            UserProfile.objects.create(user=user)
+            login(request, user)
+            messages.success(request, 'Registration successful!')
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
+    return render(request, 'accounts/profile.html', {'profile': profile})
