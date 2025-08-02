1df9c42c6d6aaeac24002cd0188cdaac654838ca
YourGitHubUsername <jmwaura@byupathway.edu>
2025-08-02 08:02:59 +0300

    COMMIT

--- progress/urls.py
+++ progress/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='progress-home'),
+    path('lesson/<int:lesson_id>/complete/', views.update_lesson_progress, name='complete_lesson'),
+    path('api/course/<int:course_id>/', views.progress_api, name='progress_api'),
 ]
+   
